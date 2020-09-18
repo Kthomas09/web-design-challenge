395a255bcdf0a52a0653c915d013bc436913f253
Kent Thomas <kent.thomas@Kents-MacBook-Air.local>
2020-09-18 15:03:07 -0400

    Fixed some kinks during tutoring. Also was able to fix the data table problem.

--- web_visualization/data.py
+++ web_visualization/data.py
@@ -2,7 +2,7 @@ import pandas as pd
 
 df = pd.read_csv("web_visualization/Resources/cities.csv")
 
-df.to_html("data.html", index= False)
+df.to_html("data_table.html", index= False)
 
 table = df.to_html()
 print(table)
